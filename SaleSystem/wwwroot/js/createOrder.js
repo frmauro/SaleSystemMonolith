9a51e77814145b6c61dec5497a0d74f6772006a7
francisco <frmauro8@hotmail.com>
2020-09-07 12:32:26 -0300

    create Order Page

--- SaleSystem/wwwroot/js/createOrder.js
+++ SaleSystem/wwwroot/js/createOrder.js
@@ -1,4 +1,82 @@
-﻿var createOrder = function () {
+﻿function addRowProduct(product) {
+    let tblItens = document.getElementById("tblItens");
+
+    let newRow = tblItens.insertRow(-1);
+
+    let cellId = newRow.insertCell(0);
+    let idText = document.createTextNode(product.id);
+    cellId.appendChild(idText);
+
+    let cellDescription = newRow.insertCell(1);
+    let descriptionText = document.createTextNode(product.description);
+    cellDescription.appendChild(descriptionText);
+
+    let cellDelete = newRow.insertCell(2);
+    let cbSelectProduct = document.createElement('input');
+    cbSelectProduct.type = 'checkbox';
+    cbSelectProduct.name = 'cbSelectProduct';
+    cbSelectProduct.id = 'ptworkinfo' + product.id;
+    cbSelectProduct.value = product.id;
+
+    cellDelete.appendChild(cbSelectProduct);
+}
+
+
+function addRowProductSelected(product) {
+    let tblItens = document.getElementById("tblItensSelected");
+    let tbody = tblItens.getElementsByTagName('tbody')[0];
+
+    let newRow = tbody.insertRow();
+
+    let cellId = newRow.insertCell(0);
+    let idText = document.createTextNode(product.id);
+    cellId.appendChild(idText);
+
+    let cellDescription = newRow.insertCell(1);
+    let descriptionText = document.createTextNode(product.description);
+    cellDescription.appendChild(descriptionText);
+
+    let cellDelete = newRow.insertCell(2);
+    let btnDeleteProduct = document.createElement('button');
+    btnDeleteProduct.type = 'button';
+    btnDeleteProduct.name = 'btnDeleteProduct';
+    btnDeleteProduct.id = 'btnDeleteProduct' + product.id;
+    btnDeleteProduct.setAttribute("class", "btn btn-primary");
+    btnDeleteProduct.innerHTML = "Delete";  
+    btnDeleteProduct.value = 'Delete';
+    btnDeleteProduct.addEventListener('click', deleteRowProductSelected.bind(this, product), false);
+    cellDelete.appendChild(btnDeleteProduct);
+}
+
+
+
+function deleteRowProductSelected(product) {
+   let newProductsSelected = createOrder.productsSelected.filter(p => {
+        return p.id != product.id;
+   });
+    createOrder.productsSelected = newProductsSelected;
+    clearTblItensSelected();
+    updateProductsSelecteds();
+}
+
+
+function clearTblItensSelected() {
+    let tblItens = document.getElementById("tblItensSelected");
+    let tbody = tblItens.getElementsByTagName('tbody')[0];
+    tbody.innerHTML = '';
+}
+
+
+function updateProductsSelecteds() {
+    createOrder.productsSelected.map(p => {
+        addRowProductSelected(p);
+    });
+}
+
+
+
+
+var createOrder = function () {
     return {
         listProducts: function () {
             //here will stay fecht
@@ -8,7 +86,7 @@
             this.products.push({ id: 4, description: 'Products 004' });
 
             this.products.map(p => {
-                addRow(p);
+                addRowProduct(p);
             });
            
         },
@@ -22,7 +100,6 @@
             let chkboxName = 'cbSelectProduct';
             let checkboxes = document.getElementsByName(chkboxName);
             for (let i = 0; i < checkboxes.length; i++) {
-                // And stick the checked ones onto an array...
                 if (checkboxes[i].checked) {
                     this.idsProductsSelected.push(checkboxes[i].value);
                 }
@@ -36,33 +113,12 @@
                 });
             });
 
-
-            console.log(this.productsSelected);
-
+            updateProductsSelecteds();
         }
     }
 
-    function addRow(product) {
-        let tblItens = document.getElementById("tblItens");
+}();
 
-        let newRow = tblItens.insertRow(-1);
 
-        let cellId = newRow.insertCell(0);
-        let idText = document.createTextNode(product.id);
-        cellId.appendChild(idText);
 
-        let cellDescription = newRow.insertCell(1);
-        let descriptionText = document.createTextNode(product.description);
-        cellDescription.appendChild(descriptionText);
 
-        let cellDelete = newRow.insertCell(2);
-        let cbSelectProduct = document.createElement('input');
-        cbSelectProduct.type = 'checkbox';
-        cbSelectProduct.name = 'cbSelectProduct';
-        cbSelectProduct.id = 'ptworkinfo' + product.id; 
-        cbSelectProduct.value = product.id;
-
-        cellDelete.appendChild(cbSelectProduct);
-    }
-
-} ();
