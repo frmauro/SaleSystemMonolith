e729cc242dbe1e977e36f8a66a48a80d1b0df574
francisco <frmauro8@hotmail.com>
2020-09-13 13:49:18 -0300

    create test product

--- SaleSystem.XUnitTest/ProductRepositoryTest.cs
+++ SaleSystem.XUnitTest/ProductRepositoryTest.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SaleSystem.Entities.Product;
+using SaleSystem.Repository;
+using SaleSystem.Repository.Context;
 using System;
 using Xunit;
 
@@ -5,9 +10,46 @@ namespace SaleSystem.XUnitTest
 {
     public class ProductRepositoryTest
     {
-        [Fact]
-        public void Test1()
+        IProductRepository repository;
+        SaleContext context;
+
+        public ProductRepositoryTest()
         {
+
+            var serviceProvider = new ServiceCollection()
+            .AddEntityFrameworkSqlServer()
+            .BuildServiceProvider();
+
+            var builder = new DbContextOptionsBuilder<SaleContext>();
+
+            builder.UseSqlServer("Data Source=DESKTOP-TAFCSAG\\SQLEXPRESS;Initial Catalog=SaleSystemDb;User ID=sa; Password=123456")
+                    .UseInternalServiceProvider(serviceProvider);
+
+            context = new SaleContext(builder.Options);
+            //_context.Database.Migrate();
+
+            repository = new ProductRepository(context);
+        }
+
+
+        [Fact]
+        public void Save_Test()
+        {
+
+            Product product = null;
+            //var i = 1;
+
+            for (var i = 0; i < 1000; i++)
+            {
+                product = new Product
+                {
+                    Amount = 10 + i,
+                    Description = string.Concat("Product", i),
+                    Status = ProductStatus.Active
+                };
+
+                repository.Insert(product);
+            }
 
         }
     }
